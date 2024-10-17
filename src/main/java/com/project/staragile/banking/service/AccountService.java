7a09607760c2c78475a6757317ee523ba3aedbc1
Raj000007 <rajeshraj19981@gmail.com>
2024-10-18 04:58:12 +0530

    nen

--- src/main/java/com/project/staragile/banking/service/AccountService.java
+++ src/main/java/com/project/staragile/banking/service/AccountService.java
@@ -9,29 +9,43 @@ import java.util.Optional;
 
 @Service
 public class AccountService {
+
     @Autowired
     private AccountRepository accountRepository;
 
+    // Method to get account details by account number
+    public Account getAccountDetails(Long accountNo) {
+        Optional<Account> accountDetails = accountRepository.findById(accountNo);
+        if (accountDetails.isPresent()) {
+            // Use getAccountHolderName() to retrieve the name
+            String name = accountDetails.get().getAccountHolderName(); // Correct method call
+            // Further processing can be done here
+            return accountDetails.get();
+        }
+        return null; // or throw an exception if account not found
+    }
+
+    // Method to create a new account
     public Account createAccount(Account account) {
         return accountRepository.save(account);
     }
 
-    public Account updateAccount(Long accountNo, Account accountDetails) {
-        Optional<Account> account = accountRepository.findById(accountNo);
-        if (account.isPresent()) {
-            Account updatedAccount = account.get();
-            updatedAccount.setAccountHolderName(accountDetails.getAccountHolderName());
-            updatedAccount.setBalance(accountDetails.getBalance());
-            return accountRepository.save(updatedAccount);
-        }
-        return null; // or throw an exception
-    }
-
-    public Optional<Account> viewAccount(Long accountNo) {
-        return accountRepository.findById(accountNo);
-    }
-
+    // Method to delete an account by account number
     public void deleteAccount(Long accountNo) {
         accountRepository.deleteById(accountNo);
+    }
+
+    // Method to update an existing account
+    public Account updateAccount(Long accountNo, Account updatedAccount) {
+        return accountRepository.findById(accountNo)
+                .map(account -> {
+                    account.setAccountHolderName(updatedAccount.getAccountHolderName());
+                    account.setBalance(updatedAccount.getBalance());
+                    return accountRepository.save(account);
+                })
+                .orElseGet(() -> {
+                    updatedAccount.setAccountNo(accountNo);
+                    return accountRepository.save(updatedAccount);
+                });
     }
 }
