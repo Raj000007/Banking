935ea3590af9f41937ea8e9b7119deb1bba89259
Raj000007 <rajeshraj19981@gmail.com>
2024-10-18 04:53:37 +0530

    nen

--- src/main/java/com/project/staragile/banking/DataInitializer.java
+++ src/main/java/com/project/staragile/banking/DataInitializer.java
@@ -1,11 +1,10 @@
 package com.project.staragile.banking;
 
-import com.project.staragile.banking.model.Account;
-import com.project.staragile.banking.repository.AccountRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
+import com.project.staragile.banking.model.Account;
+import com.project.staragile.banking.repository.AccountRepository;
 
 @Component
 public class DataInitializer {
@@ -16,7 +15,7 @@ public class DataInitializer {
     @PostConstruct
     public void init() {
         // Preload some data into the database
-        accountRepository.save(new Account(null, "John Doe", 1000.0));
-        accountRepository.save(new Account(null, "Jane Smith", 2000.0));
+        accountRepository.save(new Account("John Doe", 1000.00));
+        accountRepository.save(new Account("Jane Smith", 1500.00));
     }
 }
